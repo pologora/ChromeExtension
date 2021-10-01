6f995e7a6a252c9ab5a6162720b342469d401e53
Oleksandr Lysakov <lysakova@yahoo.com>
2021-10-01 05:19:28 +0200

    add list

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+let myLeads = []
 
 
-const saveBtn = () => {
-    console.log("cklie")
+const saveLead = () => {
+   myLeads.push(inputEl.value)
+   inputEl.value = ""
+   console.log(myLeads)
 }
+
+for(let lead of myLeads){
+   
+}
+
+inputBtn.addEventListener("click", saveLead)
