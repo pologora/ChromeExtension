004735730b82f8b8668f564f50e6452ed82b6bf9
Oleksandr Lysakov <lysakova@yahoo.com>
2021-10-04 16:56:35 +0200

    add style to links

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+
 let myLeads = []
 
 
 const saveLead = () => {
-   myLeads.push(inputEl.value)
-   inputEl.value = ""
-   console.log(myLeads)
+   if (inputEl.value) {
+      myLeads.push(inputEl.value)
+
+      inputEl.value = ""
+      console.log(myLeads)
+      renderList()
+   }
 }
 
-for(let lead of myLeads){
-   
+const renderList = () => {
+   let listItems = ""
+   for (let i = 0; i < myLeads.length; i++) {
+      // listItems += "<li>" + "<a target='_blank' href='#'>" + myLeads[i] + "</a>" + "</li>"
+      listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
+   }
+   ulEl.innerHTML = listItems
 }
+
+
 
 inputBtn.addEventListener("click", saveLead)
