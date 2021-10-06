d7449e198287a6473a46d716c3c90a3108224219
Oleksandr Lysakov <lysakova@yahoo.com>
2021-10-06 18:49:29 +0200

    deploy version

--- app.js
+++ app.js
@@ -2,27 +2,25 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 let myLeads = []
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-const renderLeads = () => {
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+const render = (leads) => {
    let listItems = ""
-   for (let i = 0; i < myLeads.length; i++) {
-      listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
+   for (let i = 0; i < leads.length; i++) {
+      listItems += `<li><a target='_blank' href='${leads[i]}'>${leads[i]}</a></li>`
    }
    ulEl.innerHTML = listItems
 }
 
 if (leadsFromLocalStorage) {
    myLeads = leadsFromLocalStorage
-   renderLeads()
+   render(myLeads)
 }
-
-// myLeads = JSON.parse(myLeads)
-
-// myLeads.push("www.second.com")
-// myLeads = JSON.stringify(myLeads)
-
 
 
 const saveLead = () => {
@@ -30,11 +28,29 @@ const saveLead = () => {
       myLeads.push(inputEl.value)
       inputEl.value = ""
       localStorage.setItem("myLeads", JSON.stringify(myLeads))
-      renderLeads()
+      render(myLeads)
    }
 }
 
+const deleteLeads = () => {
+   localStorage.clear()
+   myLeads = []
+   render(myLeads)
+}
 
 
+const tabLead = () => {
 
+   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+
+      myLeads.push(tabs[0].url)
+      localStorage.setItem("myLeads", JSON.stringify(myLeads))
+      render(myLeads)
+   })
+
+}
+
+
+deleteBtn.addEventListener("dblclick", deleteLeads)
 inputBtn.addEventListener("click", saveLead)
+tabBtn.addEventListener("click", tabLead)
