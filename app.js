82d4fe2175bef2b1e2fae77f82aa51f3166e6513
Oleksandr Lysakov <lysakova@yahoo.com>
2021-10-06 06:47:27 +0200

    add icon, manifest.json

--- app.js
+++ app.js
@@ -2,29 +2,38 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-
-
 let myLeads = []
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const renderLeads = () => {
+   let listItems = ""
+   for (let i = 0; i < myLeads.length; i++) {
+      listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
+   }
+   ulEl.innerHTML = listItems
+}
+
+if (leadsFromLocalStorage) {
+   myLeads = leadsFromLocalStorage
+   renderLeads()
+}
+
+// myLeads = JSON.parse(myLeads)
+
+// myLeads.push("www.second.com")
+// myLeads = JSON.stringify(myLeads)
+
 
 
 const saveLead = () => {
    if (inputEl.value) {
       myLeads.push(inputEl.value)
-
       inputEl.value = ""
-      console.log(myLeads)
-      renderList()
+      localStorage.setItem("myLeads", JSON.stringify(myLeads))
+      renderLeads()
    }
 }
 
-const renderList = () => {
-   let listItems = ""
-   for (let i = 0; i < myLeads.length; i++) {
-      // listItems += "<li>" + "<a target='_blank' href='#'>" + myLeads[i] + "</a>" + "</li>"
-      listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
-   }
-   ulEl.innerHTML = listItems
-}
 
 
 
